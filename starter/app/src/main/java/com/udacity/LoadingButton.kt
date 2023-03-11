865a829cf79a56a60dcc05262be554376856c58d
unknown <sanchezclarisse@gmail.com>
2023-03-11 21:33:36 +0800

    Draw download button

--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -3,9 +3,14 @@ package com.udacity
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
 import kotlin.properties.Delegates
+
+private lateinit var frame: Rect
 
 class LoadingButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
@@ -19,6 +24,22 @@ class LoadingButton @JvmOverloads constructor(
 
     }
 
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorPrimary, null)
+    private val textColor = ResourcesCompat.getColor(resources, R.color.white, null)
+
+
+    // Set up the paint with which to draw.
+    private val paint = Paint().apply {
+        color = backgroundColor
+        // Smooths out edges of what is drawn without affecting shape.
+        isAntiAlias = true
+        // Dithering affects how colors with higher-precision than the device are down-sampled.
+        isDither = true
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        textSize = 70f
+    }
+
 
     init {
 
@@ -27,7 +48,18 @@ class LoadingButton @JvmOverloads constructor(
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        canvas?.drawRect(frame, paint)
 
+        paint.color = textColor
+        canvas?.drawText(
+            "Download", (width / 2).toFloat(), ((height + 25) / 2).toFloat(),
+            paint
+        )
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        frame = Rect(0, 0, width, height)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
