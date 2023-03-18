b28fa810c97d4c574539e03e09fce1770de682f4
unknown <sanchezclarisse@gmail.com>
2023-03-18 21:35:22 +0800

    cancel notification once action button is clicked

--- starter/app/src/main/java/com/udacity/DetailActivity.kt
+++ starter/app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,7 +1,9 @@
 package com.udacity
 
+import android.app.NotificationManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_detail.*
 import kotlinx.android.synthetic.main.content_detail.*
 
@@ -14,13 +16,19 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+
+        //delete current notification
+        val notificationManager = ContextCompat.getSystemService(applicationContext, NotificationManager::class.java) as NotificationManager
+        notificationManager.cancelAll()
+
+        //get extras from intent
         if (intent != null) {
             fileName = intent.getStringExtra(getString(R.string.intent_filename)).toString()
             status = intent.getStringExtra(getString(R.string.intent_status)).toString()
         }
 
+        //set text
         txt_filename.text = fileName
         txt_status.text = status
         if (status == getString(R.string.success)) {
@@ -28,6 +36,7 @@ class DetailActivity : AppCompatActivity() {
         } else {
             txt_status.setTextColor(getColor(R.color.red))
         }
+
     }
 
 }
